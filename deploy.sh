8c43cef3630f64782f201ebd76327cc110a92641
consolelog <leopportunity5@gmail.com>
2025-01-21 16:07:19 +0900

    2025-01-21 :: fix sh file

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ IMAGE_ID=$(sudo docker images -q $IMAGE_NAME)
 if [ -z "$EXIST_BLUE" ]; then
   echo "blue up"
   sudo docker-compose -p $BITHUMB_CONTAINER_NAME-blue -f docker-compose.blue.yaml up -d --build
+
   BEFORE_COMPOSE_COLOR="green"
   AFTER_COMPOSE_COLOR="blue"
 else
@@ -20,9 +21,9 @@ fi
 
 sleep 10
 
-EXIST_AFTER=$(sudo docker-compose -p autotrading-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yaml ps | grep Up)
+EXIST_AFTER=$(sudo docker-compose -p $BITHUMB_CONTAINER_NAME-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yaml ps | grep Up)
 if [ -n "$EXIST_AFTER" ]; then
-  sudo docker-compose -p autotrading-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yaml down
+  sudo docker-compose -p $BITHUMB_CONTAINER_NAME-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yaml down
   echo "$BEFORE_COMPOSE_COLOR down"
   sudo docker rmi "$IMAGE_ID"
 fi
