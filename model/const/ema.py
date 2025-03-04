507f26f827ec9d76eccf1c94cc9e4b7eb925c64e
consolelog <leopportunity5@gmail.com>
2025-03-04 17:27:34 +0900

    delete: rollback

--- model/const/ema.py
+++
@@ -1,5 +0,0 @@
-
-class EMA:
-    SHORT = "EMA_SHORT"
-    MID = "EMA_MID"
-    LONG = "EMA_LONG"
--- server.py
+++ server.py
@@ -1,19 +1,6 @@
-from contextlib import asynccontextmanager
-
-from fastapi import FastAPI
-
-from scheduler import scheduler
-from logger import LoggerFactory
-
-logger = LoggerFactory().get_logger("server")
-
-@asynccontextmanager
-async def lifespan(app):
-    logger.info("========================")
-    logger.info("        START UP        ")
-    logger.info("========================")
-    scheduler.start()
-    yield
-app = FastAPI(lifespan=lifespan)
 
 
+
+
+if __name__ == '__main__':
+    pass
