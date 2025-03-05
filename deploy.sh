f9b95a88da80db0ab4fa7d6e6d196c1d58c7a845
consolelog <leopportunity5@gmail.com>
2025-03-05 17:45:37 +0900

    fix: duplicate

--- deploy.sh
+++ deploy.sh
@@ -22,24 +22,6 @@ else
     UPBIT_AFTER_COMPOSE_COLOR="green"
 fi
 
-EXIST_BITHUMB_BLUE=$(sudo docker-compose -p "$BITHUMB_CONTAINER_NAME-blue" -f docker-compose.blue.yaml ps | grep Up)
-
-if [ -z "$EXIST_BITHUMB_BLUE" ]; then 
-    echo "-----------------"
-    echo " bithumb blue up "
-    echo "-----------------"
-    sudo docker-compose -p "$BITHUMB_CONTAINER_NAME-blue" -f docker-compose.blue.yaml up -d --build 
-    BITHUMB_BEFORE_COMPOSE_COLOR="green"
-    BITHUMB_AFTER_COMPOSE_COLOR="blue"
-else
-    echo "------------------"
-    echo " bithumb green up "
-    echo "------------------"
-    sudo docker-compose -p "$BITHUMB_CONTAINER_NAME-green" -f docker-compose.green.yaml up -d --build 
-    BITHUMB_BEFORE_COMPOSE_COLOR="blue"
-    BITHUMB_AFTER_COMPOSE_COLOR="green"
-fi
-
 IMAGE_ID=$(sudo docker images -q $IMAGE_NAME)
 
 sleep 5
@@ -49,15 +31,6 @@ if [ -n "$UPBIT_EXIST_AFTER" ]; then
     sudo docker-compose -p "$UPBIT_CONTAINER_NAME-$UPBIT_BEFORE_COMPOSE_COLOR" -f docker-compose.$UPBIT_BEFORE_COMPOSE_COLOR.yaml down
     echo "------------------"
     echo " upbit $UPBIT_BEFORE_COMPOSE_COLOR down"
-    echo "------------------"
-    sudo docker rmi "$IMAGE_ID"
-fi
-
-BITHUMB_EXIST_AFTER=$(sudo docker-compose -p "$BITHUMB_CONTAINER_NAME-$BITHUMB_AFTER_COMPOSE_COLOR" -f docker-compose.$BITHUMB_AFTER_COMPOSE_COLOR.yaml ps | grep Up)
-if [ -n "$BITHUMB_EXIST_AFTER" ]; then
-    sudo docker-compose -p "$BITHUMB_CONTAINER_NAME-$BITHUMB_BEFORE_COMPOSE_COLOR" -f docker-compose.$BITHUMB_BEFORE_COMPOSE_COLOR.yaml down
-    echo "------------------"
-    echo " bithumb $BITHUMB_BEFORE_COMPOSE_COLOR down"
     echo "------------------"
     sudo docker rmi "$IMAGE_ID"
 fi
