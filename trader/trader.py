0580b1ea1b8fd89016d603d6e8319826027fb408
consoleolog <leopportunity5@gmail.com>
2025-04-28 18:01:35 +0900

    fix: change

--- trader/trader.py
+++ trader/trader.py
@@ -236,7 +236,7 @@ class Trader:
                     macd_info.short_cross == const.golden_cross,
                     macd_info.mid_cross == const.golden_cross,
                     macd_info.long_cross == const.golden_cross,
-                    stage == 4,
+                    # stage == 4,
                 ]
             )
             if buy_condition:
@@ -252,7 +252,7 @@ class Trader:
                     macd_info.short_cross == const.golden_cross,
                     macd_info.mid_cross == const.golden_cross,
                     macd_info.long_cross == const.golden_cross,
-                    stage == 4,
+                    # stage == 4,
                 ]
             )
             if buy_condition:
