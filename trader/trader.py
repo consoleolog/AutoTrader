6a415e682ddca803469c3c82bda743c0809dc0f9
consoleolog <167042699+consoleolog@users.noreply.github.com>
2025-06-16 08:54:39 +0900

    Update trader.py

--- trader/trader.py
+++ trader/trader.py
@@ -220,7 +220,7 @@ class Trader:
                 [
                     rsi_info.rsi_cross == const.golden_cross,
                     stochastic_info.stochastic_over == const.over_sold,
-                    stochastic_info.stochastic_over == const.golden_cross,
+                    stochastic_info.stochastic_cross == const.golden_cross,
                     macd_info.short_cross == const.golden_cross,
                     macd_info.mid_cross == const.golden_cross,
                     macd_info.long_cross == const.golden_cross,
